ecd1c69c823bf52a561a6dd9cb6b533e4efc30d2
nikhileshp <prabhakar.nikhilesh@gmail.com>
2025-08-28 12:34:39 -0500

    Changes to object detection

--- retrieve_objects.py
+++ retrieve_objects.py
@@ -17,21 +17,37 @@ enemy_colors = {"green": [92, 186, 92], "orange": [[58, 108, 198], [111, 111, 11
 def above_water(obj):
     return obj[1] < 47
 
-def left_of(obj1, obj2):
-    """Check if obj1 is to the left of obj2."""
-    return obj2[0] + obj2[2] < obj1[0]
-
 def right_of(obj1, obj2):
+    """Check if obj1 is to the left of obj2."""
+    return obj2[0] + obj2[2] <= obj1[0]
+
+def left_of(obj1, obj2):
     """Check if obj1 is to the right of obj2."""
-    return obj1[0] + obj1[2] < obj2[0]
+    return obj1[0] + obj1[2] <= obj2[0]
 
-def below_of(obj1, obj2):
+
+# Add three functions above_of, below_of and same_level_of to check if obj1 is above, below or at the same level as obj2
+# Obj1 is on the same level of obj2 if the height of the body of obj2 is mostly overlapping the hight of the body of obj1
+def above_of(obj1, obj2, min_coverage=0.51):
     """Check if obj1 is above obj2."""
-    return obj1[1] < obj2[3] + obj2[1]
+    mid_y1 = obj1[1] + obj1[3] // 2
+    coverage = max(0, min(obj1[1] + obj1[3], obj2[1] + obj2[3]) - max(obj1[1], obj2[1]))
+    coverage_ratio = coverage / obj2[3]
+    return mid_y1 < obj2[1] and (coverage_ratio < min_coverage)
 
-def above_of(obj1, obj2):
+def below_of(obj1, obj2, min_coverage=0.51):
     """Check if obj1 is below obj2."""
-    return obj1[1] > obj2[1] + obj2[3]
+    mid_y1 = obj1[1] + obj1[3] // 2
+    coverage = max(0, min(obj1[1] + obj1[3], obj2[1] + obj2[3]) - max(obj1[1], obj2[1]))
+    coverage_ratio = coverage / obj2[3]
+    return mid_y1 > (obj2[1] + obj2[3]) and (coverage_ratio < min_coverage)
+    
+def same_level_of(obj1, obj2, min_coverage=0.51):
+    """Check if obj1 is at the same level as obj2."""
+    mid_y1 = obj1[1] + obj1[3] // 2
+    coverage = max(0, min(obj1[1] + obj1[3], obj2[1] + obj2[3]) - max(obj1[1], obj2[1]))
+    coverage_ratio = coverage / obj2[3]
+    return (obj2[1] <= mid_y1 <= (obj2[1] + obj2[3])) and (coverage_ratio >= min_coverage)
 
 class Player(GameObject):
     def __init__(self, *args, **kwargs):
@@ -179,7 +195,9 @@ if __name__ == '__main__':
             'duration': duration,
             'unclipped_reward': unclipped_reward,
             'action': action,
-            'gaze_positions': gaze_positions
+            'gaze_positions': gaze_positions,
+            "objects": "",
+            'relationships': ""
         })
     gaze_df = pd.DataFrame(gaze_info)
 
@@ -194,17 +212,16 @@ if __name__ == '__main__':
         #Still unable to find in some frames, need better method
 
         enemy = []
-
+        orange_yellow = enemy_colors["orange"] + enemy_colors["yellow"]
         enemy.extend(find_objects(obs, enemy_colors["green"], maxy=150, miny=40))
-        enemy.extend(find_objects(obs, enemy_colors["orange"], maxy=150, miny=40))
-        enemy.extend(find_objects(obs, enemy_colors["yellow"], maxy=150, miny=40))
         enemy.extend(find_objects(obs, enemy_colors["lightgreen"], maxy=150, miny=40))
-        enemy.extend(find_objects(obs, enemy_colors["pink"], maxy=150, miny=40)) 
+        enemy.extend(find_objects(obs, enemy_colors["pink"], maxy=150, miny=40))    
+        enemy.extend(find_objects(obs, orange_yellow, maxy=150, miny=40))
 
         # Print the image number and found objects
         print(f"Processing {img_name}:")
-
-        player = find_objects(obs, objects_colors["player"], size=(15,10), tol_s=(10,8))
+        
+        player = find_objects(obs, objects_colors["player"], size=(15,10), tol_s=(14,8))
 
 
 
@@ -217,12 +234,20 @@ if __name__ == '__main__':
         missiles = find_objects(obs, objects_colors["player_missile"], miny=40, maxy=150, size=(12,1), tol_s=(5,1))
         enemy_missiles = find_objects(obs, objects_colors["enemy_missile"], size=(8,4), tol_s=1, minx=20, miny=90, maxy=310)
         
+        obj_remove_list = []
         for obj in enemy_missiles:
             for diver in divers:
-                if obj[0] >= diver[0] and obj[0] <= diver[0] + diver[2] and obj[1] >= diver[1] and obj[1] <= diver[1] + diver[3]:
+                if obj[0] >= diver[0] and obj[0] <= (diver[0] + diver[2]) and obj[1] >= diver[1] and obj[1] <= (diver[1] + diver[3]):
                     # If the enemy missile is in the bounding box of the diver, then remove it
-                    enemy_missiles.remove(obj)
-                    break   
+                    obj_remove_list.append(obj) 
+        
+        for diver in divers:
+            if (diver[2] >= 6 and diver[2] <= 8) and diver[3] == 4:
+                    enemy_missiles.append(diver)
+
+        for obj in obj_remove_list:
+            if obj in enemy_missiles:
+                enemy_missiles.remove(obj) 
 
         
         for obj in missiles:
@@ -238,7 +263,10 @@ if __name__ == '__main__':
         
         # print("Found lives:", lives)
 
-        enemy_submarine = find_objects(obs, objects_colors["submarine"], size=(8,10), tol_s=2, maxy=150, min_distance=1)
+        enemy_submarine = find_objects(obs, objects_colors["submarine"], size=(8,10), tol_s=4, miny=56, maxy=150, min_distance=1)
+        submarine_on_water = find_objects(obs, objects_colors["submarine"], miny=40, maxy=55, min_distance=1)
+
+        enemy_submarine.extend(submarine_on_water)
         # print("Found enemy submarines:", enemy_submarine)
         # Unable to find any oxygen bar, it should be in a red rectangle
         oxygen_bar = find_objects(
@@ -265,65 +293,75 @@ if __name__ == '__main__':
         lives_dict = {}
         oxygen_bar_dict = {}
         oxygen_depleted_dict = {}
-        enemy_submarine_dict = {}
+        
         enemy_dict = {}
-
-
+        objects_list = []
+        
         for i, obj in enumerate(player):
+            objects_list.append(f"player_{i}")
             if players_dict:
                 players_dict[f"player_{i}"] = obj
             else:
                 players_dict = {f"player_{i}": obj}
 
         for i, obj in enumerate(divers):
+            objects_list.append(f"diver_{i}")
             if divers_dict:
                 divers_dict[f"diver_{i}"] = obj
             else:
                 divers_dict = {f"diver_{i}": obj}
 
         for i, obj in enumerate(coll_diver):
+            objects_list.append(f"coll_diver_{i}")
             if coll_diver_dict:
                 coll_diver_dict[f"coll_diver_{i}"] = obj
             else:
                 coll_diver_dict = {f"coll_diver_{i}": obj}
         
         for i, obj in enumerate(missiles):
+            objects_list.append(f"missile_{i}")
             if missiles_dict:
                 missiles_dict[f"missile_{i}"] = obj
             else:
                 missiles_dict = {f"missile_{i}": obj}
 
         for i, obj in enumerate(enemy_missiles):
-            if enemy_missiles:
+            objects_list.append(f"enemy_missile_{i}")
+            if enemy_dict:
                 enemy_dict[f"enemy_missile_{i}"] = obj
             else:
                 enemy_dict = {f"enemy_missile_{i}": obj}
 
         for i, obj in enumerate(lives):
+            objects_list.append(f"life_{i}")
             if lives_dict:
                 lives_dict[f"life_{i}"] = obj
             else:
                 lives_dict = {f"life_{i}": obj}
 
         for i, obj in enumerate(oxygen_bar):
+            objects_list.append(f"oxygen_bar_{i}")
             if oxygen_bar_dict:
                 oxygen_bar_dict[f"oxygen_bar_{i}"] = obj
             else:
                 oxygen_bar_dict = {f"oxygen_bar_{i}": obj}
 
         for i, obj in enumerate(oxygen_depleted):
+            objects_list.append(f"oxygen_depleted_{i}")
             if oxygen_depleted_dict:
                 oxygen_depleted_dict[f"oxygen_depleted_{i}"] = obj
             else:
                 oxygen_depleted_dict = {f"oxygen_depleted_{i}": obj}
 
         for i, obj in enumerate(enemy_submarine):
-            if enemy_submarine_dict:
-                enemy_submarine_dict[f"enemy_submarine_{i}"] = obj
+            objects_list.append(f"enemy_submarine_{i}")
+            if enemy_dict:
+                enemy_dict[f"enemy_submarine_{i}"] = obj
             else:
-                enemy_submarine_dict = {f"enemy_submarine_{i}": obj}
+                enemy_dict = {f"enemy_submarine_{i}": obj}
 
         for i, obj in enumerate(enemy):
+            objects_list.append(f"enemy_{i}")
             if enemy_dict:
                 enemy_dict[f"enemy_{i}"] = obj
             else:
@@ -370,10 +408,7 @@ if __name__ == '__main__':
         else:
             print("Oxygen Depleted objects:", oxygen_depleted_dict)
 
-        if not enemy_submarine:
-            pass
-        else:
-            print("Enemy Submarine objects:", enemy_submarine_dict)
+
         
         if not enemy:
             pass
@@ -383,41 +418,102 @@ if __name__ == '__main__':
         
         print()
         print("Relationship between objects:")
+        relationships = []
+        try:
+            # Check if player is above water
+            if player and above_water(player[0]):
+                print("aboveWater(player).")
+                relationships.append((player[0], "water_surface", "aboveWater", "player"))
+            else:
+                print("belowWater(player).")
+                relationships.append((player[0], "water_surface", "belowWater", "player"))
+            # Check if player is to the left of any diver
+            # Add relationships to a list with tuple information in the form of (obj1coord, obj2coord, relationship)
+            
+        
+       
+            for diver, coords in divers_dict.items():
+            
+                if left_of(player[0], coords):
+                    relationships.append((player[0], coords, "leftOf", "player", diver))
+                    print("leftOf(player,", diver, ").")
 
-        # Check if player is above water
-        if player and above_water(player[0]):
-            print("aboveWater(player).")
-        else:
-            print("belowWater(player).")
-        # Check if player is to the left of any diver
-        for diver, coords in divers_dict.items():
+                elif right_of(player[0], coords):
+                    relationships.append((player[0], coords, "rightOf", "player", diver))
+                    print("rightOf(player,", diver, ").")
 
-            if left_of(player[0], coords):
-                print("leftOf(player,", diver, ").")
+                if above_of(player[0], coords):
+                    relationships.append((player[0], coords, "aboveOf", "player", diver))
+                    print("aboveOf(player,", diver, ").")
 
-            elif right_of(player[0], coords):
-                print("rightOf(player,", diver, ").")
+                elif below_of(player[0], coords):
+                    relationships.append((player[0], coords, "belowOf", "player", diver))
+                    print("belowOf(player,", diver, ").")
+                
+                else:
+                    relationships.append((player[0], coords, "sameLevelAs", "player", diver))
+                    print("sameLevelAs(player,", diver, ").")
+            
+        
 
-            if above_of(player[0], coords):
-                print("aboveOf(player,", diver, ").")
+            for enemy_obj, coords in enemy_dict.items():
+                if left_of(player[0], coords):
+                    relationships.append((player[0], coords, "leftOf", "player", enemy_obj))
+                    print("leftOf(player,", enemy_obj, ").")
 
-            elif below_of(player[0], coords):
-                print("belowOf(player,", diver, ").")
+                elif right_of(player[0], coords):
+                    relationships.append((player[0], coords, "rightOf", "player", enemy_obj))
+                    print("rightOf(player,", enemy_obj, ").")
 
-        for enemy_obj, coords in enemy_dict.items():
-            if left_of(player[0], coords):
-                print("leftOf(player,", enemy_obj, ").")
+                if above_of(player[0], coords):
+                    relationships.append((player[0], coords, "aboveOf", "player", enemy_obj))
+                    print("aboveOf(player,", enemy_obj, ").")
 
-            elif right_of(player[0], coords):
-                print("rightOf(player,", enemy_obj, ").")
+                elif below_of(player[0], coords):
+                    relationships.append((player[0], coords, "belowOf", "player", enemy_obj))
+                    print("belowOf(player,", enemy_obj, ").")
+                
+                else:
+                    relationships.append((player[0], coords, "sameLevelAs", "player", enemy_obj))
+                    print("sameLevelAs(player,", enemy_obj, ").")
 
-            if above_of(player[0], coords):
-                print("aboveOf(player,", enemy_obj, ").")
+        except Exception as e:
+            print("Error in finding relationships:", e)
+            relationships = []
+                # Save new dataframe to a new text file in the same directory as the original text file with _with_relationships appended to the filename
+            new_text_file_path = text_file_path.replace(".txt", "_with_relationships.txt")
+            # Remove gaze_positions column from the dataframe before saving
+            gaze_df = gaze_df.drop(columns=['gaze_positions'])
+            gaze_df.to_csv(new_text_file_path, index=False)
 
-            elif below_of(player[0], coords):
-                print("belowOf(player,", enemy_obj, ").")
-
-
+            print(f"Saved updated gaze data with relationships to {new_text_file_path}")
+             # # Show a higher resolution window
+            obs = cv2.resize(obs, (width*2, height*2), interpolation=cv2.INTER_NEAREST)
+        
+             # Run all frames in a loop without expecting a key press
+            cv2.imshow('Frame', obs)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            quit()
+                
+        # Using the relationship list, make a connection list which lists the connection between objects in the form of (objcoord1, objcoord2, [list of relationships])
+        # Add the distance between the two objects in the connection list as well
+        connection_list = []
+        for rel in relationships:
+            found = False
+            for conn in connection_list:
+                if rel[0] == conn[0] and rel[1] == conn[1]:
+                    conn[2].append(rel[2])
+                    
+                    found = True
+                    break
+            if not found:
+                if "water" in rel[1]:
+                    continue
+                distance = ((rel[0][0] - rel[1][0]) ** 2 + (rel[0][1] - rel[1][1]) ** 2) ** 0.5
+                connection_list.append([rel[0], rel[1], [rel[2]], distance])
+        print("Connection list:", connection_list)
+        
         # Mark found objects on the image
         for obj_list, color in [(player, (0, 255, 0)),
                                 (divers, (255, 0, 0)),
@@ -439,6 +535,18 @@ if __name__ == '__main__':
                 mark_bb(obs, obj, color=color)
                 # pass
 
+        # For every connection in the connection list, draw a black line between the center of the two objects
+        # Add text in the middle of the line with the relationships in format (capitalized first letter of each relationship separated by hyphen)
+        for conn in connection_list:
+            obj1, obj2 = conn[0], conn[1]
+            center1 = (obj1[0] + obj1[2] // 2, obj1[1] + obj1[3] // 2)
+            center2 = (obj2[0] + obj2[2] // 2, obj2[1] + obj2[3] // 2)
+            cv2.line(obs, center1, center2, (0, 0, 0), 1)
+            mid_point = ((center1[0] + center2[0]) // 2, (center1[1] + center2[1]) // 2)
+            relationships_text = '-'.join([rel[0].upper()[0] for rel in conn[2]])
+            cv2.putText(obs, relationships_text, mid_point, cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 0), 1)
+
+
         # Mark the gaze positions on the image
         if not gaze_df.empty:
             #Match the frame id from the image name
@@ -447,7 +555,23 @@ if __name__ == '__main__':
             frame_id = img_name.strip(".png")
             # print("Frame ID:", frame_id)
             
-
+            # Add the relationships to the dataframe
+            for obj in objects_list:
+                if gaze_df.loc[gaze_df['frameid'] == frame_id, 'objects'].empty:
+                    gaze_df.loc[gaze_df['frameid'] == frame_id, 'objects'] = f"{obj} , "
+                else:
+                    gaze_df.loc[gaze_df['frameid'] == frame_id, 'objects'] += f"{obj} , "
+            for rel in relationships:
+                if gaze_df.loc[gaze_df['frameid'] == frame_id, 'relationships'].empty:
+                    if len(rel) ==4:
+                        gaze_df.loc[gaze_df['frameid'] == frame_id, 'relationships'] = f"{rel[2]}({rel[3]}) , "
+                    elif len(rel) ==5:
+                        gaze_df.loc[gaze_df['frameid'] == frame_id, 'relationships'] = f"{rel[2]}({rel[3]},{rel[4]}) , "
+                else:
+                    if len(rel) ==4:
+                        gaze_df.loc[gaze_df['frameid'] == frame_id, 'relationships'] += f"{rel[2]}({rel[3]}) , "
+                    elif len(rel) ==5:
+                        gaze_df.loc[gaze_df['frameid'] == frame_id, 'relationships'] += f"{rel[2]}({rel[3]},{rel[4]}) , "
             gaze_positions = gaze_df[gaze_df['frameid'] == frame_id]['gaze_positions'].values
             if len(gaze_positions) > 0:
                 # Mark the tuples of gaze positions on the image
@@ -459,14 +583,30 @@ if __name__ == '__main__':
                             
                             cv2.circle(obs, (x, y), 1 , (0, 0, 255), -1)
         
-  
+
         # # Show a higher resolution window
         obs = cv2.resize(obs, (width*2, height*2), interpolation=cv2.INTER_NEAREST)
         
-        # window_name = 'Frame'
+        # Run all frames in a loop without expecting a key press
         cv2.imshow('Frame', obs)
-        key = cv2.waitKey(0)  # Wait for a key press to move
-        if key == 27:
-            break
-    cv2.destroyAllWindows()
+        cv2.waitKey(1)  # Display each frame for 1 ms
         
+
+        # window_name = 'Frame'
+        # cv2.imshow('Frame', obs)
+        # key = cv2.waitKey(0)  # Wait for a key press to move
+        # if key == 27:
+        #     break
+    
+    cv2.destroyAllWindows()
+
+
+        
+    # Save new dataframe to a new text file in the same directory as the original text file with _with_relationships appended to the filename
+    new_text_file_path = text_file_path.replace(".txt", "_with_relationships.txt")
+    # Remove gaze_positions column from the dataframe before saving
+    gaze_df = gaze_df.drop(columns=['gaze_positions'])
+    gaze_df.to_csv(new_text_file_path, index=False)
+
+    print(f"Saved updated gaze data with relationships to {new_text_file_path}")
+    
